d4e788a425cd1e1b62c45a56c702e647c79a46e2
vvarsha23 <varravarsha@gmail.com>
2025-08-10 10:48:48 +0530

    Delete 8-08-2025_Exercise2/LetsGohaving directory

--- 8-08-2025_Exercise2/LetsGohaving/src/main/java/com/wipro/LetsGohaving/repository/VehicleMovementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.LetsGohaving.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.LetsGohaving.model.VehicleMovement;
-
-public interface VehicleMovementRepository extends JpaRepository<VehicleMovement, Long> {
-
-}
