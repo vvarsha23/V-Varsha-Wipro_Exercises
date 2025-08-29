e3429dd872361fac790fb34919bebff810fe58ba
vvarsha23 <varravarsha@gmail.com>
2025-08-29 16:26:46 +0530

    Add files via upload

---
+++ captsone_project/Backend/eureka-server/src/main/java/com/wipro/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
