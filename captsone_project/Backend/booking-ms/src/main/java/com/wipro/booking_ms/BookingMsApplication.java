e37430ad4a8709146ed1ba087253337e8fd34784
vvarsha23 <varravarsha@gmail.com>
2025-08-29 15:33:53 +0530

    Add files via upload

---
+++ captsone_project/Backend/booking-ms/src/main/java/com/wipro/booking_ms/BookingMsApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.booking_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingMsApplication.class, args);
+	}
+
+}
