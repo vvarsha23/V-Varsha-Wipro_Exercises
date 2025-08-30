e1fd27e66c2fbd814bb930705dbfba8dc64b725b
vvarsha23 <varravarsha@gmail.com>
2025-08-30 20:37:01 +0530

    Delete captsone_project/Backend/flightdata-ms directory

--- captsone_project/Backend/flightdata-ms/src/main/java/com/wipro/flightdata_ms/service/FlightService.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.flightdata_ms.service;
-import java.time.LocalDate;
-import java.util.List;
-
-import com.wipro.flightdata_ms.entity.Flight;
-public interface FlightService {
-	List<Flight> searchFlights(String source, String destination, LocalDate date);
-
-}
